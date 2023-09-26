10c9872d85b525eeac72cb9d080310e868283725
zhangtao25 <wr_zhang25@163.com>
2023-09-26 11:30:13 +0000

    Deploying to gh-pages from @ zhangtao25/arex-remocollab@b39ed5482c1f4fbae6a5042e0680ba96ddbd8c42 🚀

---
+++ assets/save-b6307bef.js
@@ -0,0 +1,1 @@
+import{r as s,a as o,j as a,B as r}from"./index-dfda4249.js";import{u as n,t as u,S as l}from"./mock-fe08f9af.js";const p=()=>{const[t,e]=s.useState(!0);return n(),o("div",{children:[a(r,{onClick:()=>{e(!0)},children:"Save As"}),a(l,{treeData:u,open:t,requestName:"",onCreateFolder:()=>{},onSave:d=>{},onClose:()=>{e(!1)}})]})};export{p as default};
