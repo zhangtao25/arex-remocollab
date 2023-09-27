61cfedd38fc7ccd0a3129c07393b768cb3b4d6e4
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2023-09-27 13:28:52 +0800

    update

--- src/pages/request.tsx
+++ src/pages/request.tsx
@@ -1,23 +1,73 @@
 import { css } from '@emotion/react';
 import { Allotment } from 'allotment';
 import { Button } from 'antd';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
+import { ArexConfigContext } from '../libs/ArexConfigProvider.tsx';
 import CollectionMenus from '../libs/menus/collection';
 import SaveRequestModal from '../libs/modal/request/save.tsx';
-// import treeData from '../mock.json';
-// import SaveRequestModal from "./modal/request/save.tsx";
 import treeData from '../mock.json';
 const Request = () => {
   const [open, setOpen] = useState(false);
+  const { state, setState } = useContext(ArexConfigContext);
+  function onChangeTheme(theme) {
+    setState({
+      ...state,
+      bgColor: theme,
+    });
+  }
+  function onChangeLocale(locale) {
+    setState({
+      ...state,
+      locale: locale,
+    });
+  }
+  const locale = state.locale;
+  const theme = state.bgColor;
+
   return (
     <div>
       <header
         css={css`
-          height: 48px;
+          height: 36px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0 10px;
           border-bottom: 1px solid #e5e7eb;
         `}
-      ></header>
+      >
+        {/*左*/}
+        <div>{JSON.stringify(state)}</div>
+
+        {/*右*/}
+        <div>
+          {['cn', 'en', 'light', 'dark'].map((item,index) => {
+            return (
+              (['cn', 'en'].includes(item) ? locale : theme) === item && (
+                <Button
+                  key={index}
+                  onClick={() => {
+                    if (['cn', 'en'].includes(item)) {
+                      onChangeLocale(item == 'cn' ? 'en' : 'cn');
+                    } else {
+                      onChangeTheme(item == 'light' ? 'dark' : 'light');
+                    }
+                  }}
+                  css={css`
+                    padding: 0 10px !important;
+                    text-align: center;
+                    margin-left: 10px;
+                  `}
+                  size={'small'}
+                >
+                  {item.toUpperCase()}
+                </Button>
+              )
+            );
+          })}
+        </div>
+      </header>
       <Allotment
         css={css`
           height: ${'calc(100vh - 80px)'};
